531a27a7a86c20f7fda7422d3a43cb2e6247c122
Samundra_pdl <samundra.pdl2000@gmail.com>
2024-04-13 22:17:27 +0545

    Adding the functionality of edit & update in redux

--- 13_reduxToolkitToDoProject/src/components/AddTodo.jsx
+++ 13_reduxToolkitToDoProject/src/components/AddTodo.jsx
@@ -8,20 +8,30 @@ function AddTodo() {
   const dispatch= useDispatch(); //This is the syntax to use the dispatch in redux..
   //Here, we are executing the action using dispatch. 
 
-  const addTodoHandler = (e) =>{
-    e.preventDefault();
-    //Dispatch does use reducers and make changes the values inside the store.. 
-    dispatch(addTodo(input)) //dispatch is our method and we're sending the value from the addTodo reducer
-    //to the addTodoHandler method above..
-    //Here, we call reducer named addTodo from our slice file with payload as input field data.
-    //Also, we've send the value to the addTodo reducer named 'input' to be used in the action creator.
-    //Now, the new todo will appear on the screen after submitting this form.
-    setInput("") //After adding a todo, clearing out the input box for next addition of todos.
+  // const addTodoHandler = (e) =>{
+  //   e.preventDefault();
+  //   //Dispatch does use reducers and make changes the values inside the store.. 
+  //   dispatch(addTodo(input)) //dispatch is our method and we're sending the value from the addTodo reducer
+  //   //to the addTodoHandler method above..
+  //   //Here, we call reducer named addTodo from our slice file with payload as input field data.
+  //   //Also, we've send the value to the addTodo reducer named 'input' to be used in the action creator.
+  //   //Now, the new todo will appear on the screen after submitting this form.
+  //   setInput("") //After adding a todo, clearing out the input box for next addition of todos.
 
+  // };
+  //In a better way;
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+    if (input.trim() !== "") {
+      dispatch(addTodo(input.trim()));
+      setInput("");
+    }
   };
 
     return (
-      <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
+      // <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
+      //In a better way;
+      <form onSubmit={handleAddTodo} className="space-x-3 mt-12">
       <input
         type="text"
         className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
--- 13_reduxToolkitToDoProject/src/features/todo/todoSlice.js
+++ 13_reduxToolkitToDoProject/src/features/todo/todoSlice.js
@@ -4,7 +4,7 @@ import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState= [
     {
-        id: 1,
+        id: nanoid(),
         text: "Go to the gym",
         completed: false,
       }
@@ -22,7 +22,10 @@ export const todoSlice =createSlice({
     //And our initial state name is todos so by naming todo we can access todos initial state..
     //Saying this for Todos.jsx where we're using const todos = useSelector((state)=> {state.todos})
     reducers:{
+        //Here, reducers is an object where we put values in key-value-function pairs format.
         addTodo:(state, action)=> {
+            //Inside state we can find updated state value in the store and inside action we can find
+            //action.payload and many more
             //Here, we've to bring our data(todo) into state using payload..
             //So, we are sending our new Todo Item as Payload..
             const newTodo ={
@@ -45,12 +48,26 @@ export const todoSlice =createSlice({
             const todoIdToRemove = action.payload;
             return state.filter(todo => todo.id !== todoIdToRemove);
         },
+        editTodo: (state, action) => {
+            const { id, newText } = action.payload;
+            const todoToEdit = state.find(todo => todo.id === id);
+            if (todoToEdit) {
+              todoToEdit.text = newText;
+            }
+          },
+          updateTodo: (state, action) => {
+            const { id, completed } = action.payload;
+            const todoToUpdate = state.find(todo => todo.id === id);
+            if (todoToUpdate) {
+              todoToUpdate.completed = completed;
+            }
+          },
     }
     //Inside of reducers, it takes property and its corresponding callback function as values.
 });
 //Here, we've exported todoSlice but actually we do export it in another way..
 
-export const {removeTodo, addTodo}= todoSlice.actions;
+export const {removeTodo, editTodo, addTodo, updateTodo }= todoSlice.actions;
 //Here, we've extracted two functions; addTodo and removeTodo from todoSlice.actions..
 //These two functions are now accessible anywhere else in our application where we import this file..
 //Also, we've to export individual reducers like removeTodo or anything you define..
