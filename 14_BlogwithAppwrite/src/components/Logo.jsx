021e44fa2e75f3f3ad3498292943fe994aab4797
Samundra_pdl <samundra.pdl2000@gmail.com>
2024-05-19 19:07:32 +0545

    Created a Logo component

---
+++ 14_BlogwithAppwrite/src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Logo({ width = '100px' }) {
+  return <div>Logo</div>;
+}
+
+export default Logo;
+
+
+// export default Logo
+//Changing import {Logo} from '../Logo'; in Footer.jsx to import Logo from '../Logo';
+//So; we do export it as object;
+// export {Logo};
