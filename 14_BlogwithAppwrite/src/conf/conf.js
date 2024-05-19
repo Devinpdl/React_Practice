8e8b1e0221670aeb7dfeb2e8ca353beef16d455b
Samundra_pdl <samundra.pdl2000@gmail.com>
2024-05-19 19:54:48 +0545

    Secure appwrite config: stringifying env

--- 14_BlogwithAppwrite/src/conf/conf.js
+++ 14_BlogwithAppwrite/src/conf/conf.js
@@ -1,12 +1,11 @@
-const conf = {
-    //Here, env variables must all be in string variables so we do convert it into string and do access it..
-
+ //Here, env variables must all be in string variables so we do convert it into string and do access it..
+ const conf = {
     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-
-}
+    // appwriteApiKey: String(import.meta.env.VITE_APPWRITE_API_KEY), // Uncomment and use if needed
+};
 
 export default conf;
