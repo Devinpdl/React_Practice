ee1126fd8f1472d4364d95cfa36aa8de6b8477e4
Samundra_pdl <samundra.pdl2000@gmail.com>
2024-05-19 19:50:29 +0545

    Configure Redux store with auth slice

--- 14_BlogwithAppwrite/src/store/store.js
+++ 14_BlogwithAppwrite/src/store/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
+import authSlice from './authSlice';
 
 const store = configureStore({
     //Inside Store there comes only one parameter reducer;
     reducer:{
-        
+        auth: authSlice,
+         //TODO: add more slices here for posts
     }
 });
 
